86d28da7b3751a6f8367778cf1651766ae5a290c
José Neto <jnognet@gmail.com>
2022-11-13 18:29:35 -0500

    corrigindo pending nat no loadbalancer

--- 003_instala_ssl.sh
+++ 003_instala_ssl.sh
@@ -1,3 +1,7 @@
+#https://github.com/nuBacuk/docker-openvpn-arm64
+sudo modprobe iptable_filter  
+sudo modprobe iptable_nat  
+
 /usr/local/bin/kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.3.1/deploy/static/provider/cloud/deploy.yaml
 
 /usr/local/bin/kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.10.0/cert-manager.yaml
