6e7d9360ceb872d367ceb225a0556bd5390889af
José Neto <jnognet@gmail.com>
2022-11-13 12:55:17 -0500

    corrige cluster

--- 002_instalar_cluster.sh
+++ 002_instalar_cluster.sh
@@ -1,8 +1,8 @@
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 kubectl version --client
 
-curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+sudo curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 k3d --version
 
 k3d cluster create k3s --api-port 6550 --k3s-arg "--disable=traefik@server:0" --wait --port "80:80@loadbalancer" --port "443:443@loadbalancer" --agents 2
