427a367f1afee6aaddfc1c7a50523065441080b9
José Neto <jnognet@gmail.com>
2022-11-13 12:04:06 -0500

    incluir sudo

--- 001_instalar_docker.sh
+++ 001_instalar_docker.sh
@@ -1,12 +1,12 @@
 #!/bin/env bash
 
-dnf update -y
-dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
-dnf install docker-ce -y
-systemctl start docker
-systemctl enable docker
-systemctl status docker
+sudo dnf update -y
+sudo dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
+sudo dnf install docker-ce -y
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo systemctl status docker
 
-curl -L https://github.com/docker/compose/releases/download/v2.5.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-docker-compose --version
+sudo curl -L https://github.com/docker/compose/releases/download/v2.5.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo docker-compose --version
