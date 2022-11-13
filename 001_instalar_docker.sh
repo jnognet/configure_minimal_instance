807e7e922ee03f545ec5168309c0cd3308a9af14
José Neto <jnognet@gmail.com>
2022-11-13 12:39:09 -0500

    remove status

--- 001_instalar_docker.sh
+++ 001_instalar_docker.sh
@@ -5,7 +5,6 @@ sudo dnf config-manager --add-repo=https://download.docker.com/linux/centos/dock
 sudo dnf install docker-ce -y
 sudo systemctl start docker
 sudo systemctl enable docker
-sudo systemctl status docker
 
 sudo curl -L https://github.com/docker/compose/releases/download/v2.5.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
