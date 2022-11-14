3c99a2afc7ec4957ae632c2ac2ec1c4567e453ff
jnognet <jnognet@gmail.com>
2022-11-13 23:40:30 -0500

    Update 002_instalar_cluster.sh

--- 002_instalar_cluster.sh
+++ 002_instalar_cluster.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/arm64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 /usr/local/bin/kubectl version --client
