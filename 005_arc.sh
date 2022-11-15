0c2f2cb844e0751fbf45111c6cd328691c9b2c56
José Neto <jnognet@gmail.com>
2022-11-15 05:14:08 -0500

    remove arc

--- 005_arc.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# https://github.com/actions-runner-controller/actions-runner-controller
-/usr/local/bin/kubectl create -f https://github.com/actions-runner-controller/actions-runner-controller/releases/download/v0.22.0/actions-runner-controller.yaml
