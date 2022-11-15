b62585a13ba1d988148a4a6384b6d8e6b8a00478
jnognet <jnognet@gmail.com>
2022-11-15 00:21:46 -0500

    ingress-nginx to 1.51v

--- 003_instala_ssl.sh
+++ 003_instala_ssl.sh
@@ -4,7 +4,7 @@
 sudo modprobe iptable_filter  
 sudo modprobe iptable_nat  
 
-/usr/local/bin/kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.3.1/deploy/static/provider/cloud/deploy.yaml
+/usr/local/bin/kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.5.1/deploy/static/provider/cloud/deploy.yaml
 
 /usr/local/bin/kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.10.0/cert-manager.yaml
 
