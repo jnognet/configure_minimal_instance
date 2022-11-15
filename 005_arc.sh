3809100677f34a8f6819ad0ba72a6a3bcf2cfdc8
José Neto <jnognet@gmail.com>
2022-11-15 01:09:34 -0500

    url arc

--- 005_arc.sh
+++ 005_arc.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+# https://github.com/actions-runner-controller/actions-runner-controller
 /usr/local/bin/kubectl create -f https://github.com/actions-runner-controller/actions-runner-controller/releases/download/v0.22.0/actions-runner-controller.yaml
